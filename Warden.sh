0d93c9b751229218ea5ea5ee8fdea2a73bfc8764
smeb y <48400087+a3165458@users.noreply.github.com>
2024-04-19 20:34:47 +0800

    Update Warden.sh

--- Warden.sh
+++ Warden.sh
@@ -254,7 +254,7 @@ function main_menu() {
     6) check_sync_status ;;
     7) check_service_status ;;
     8) view_logs ;;
-    9) uninstall_script ;;
+    9) uninstall_node ;;
     10) check_and_set_alias ;;  
     11) reward_test ;;  
     12) delegate_self_validator ;;  
